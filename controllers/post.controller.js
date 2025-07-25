b779f68b417218e299dbc7ef034ac63bd2b6576c
Lakshya Soni <sonilakshya351@gmail.com>
2025-07-25 17:31:05 +0530

    post controller completed

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,44 +2,44 @@ const { success } = require('zod');
 const Post = require('../models/post.model.js')
 const User = require('../models/user.model.js')
 
-const createPost = async (req, res)=>{
-try {
-    const {title, description, tags, body} = req.body
-
-    const wpm = 225;
-    const numberOfWords = body.trim().split(/\s+/)
-    const readTime = Math.ceil(numberOfWords/wpm)
-
-    let {name} = req.user
-    let author = name
-    let authorId = req.user._id
-    const post = await Post.create({
-        title,
-        description,
-        tags,
-        body,
-        author,
-        authorId,
-        readTime
-    })
-    let user = await user.findById(req.user._id)
-    user.posts.push(post._id)
-    await user.save()
-
-    res.status(201).json({
-        status:"success",
-        post
-    })
-} catch (error) {
-     throw err
-}
-}
-
-const allPosts = async (req, res)=>{
+const createPost = async (req, res) => {
     try {
-        const posts = await Post.find({state:"published"})
+        const { title, description, tags, body } = req.body
+
+        const wpm = 225;
+        const numberOfWords = body.trim().split(/\s+/)
+        const readTime = Math.ceil(numberOfWords / wpm)
+
+        let { name } = req.user
+        let author = name
+        let authorId = req.user._id
+        const post = await Post.create({
+            title,
+            description,
+            tags,
+            body,
+            author,
+            authorId,
+            readTime
+        })
+        let user = await user.findById(req.user._id)
+        user.posts.push(post._id)
+        await user.save()
+
+        res.status(201).json({
+            status: "success",
+            post
+        })
+    } catch (error) {
+        throw err
+    }
+}
+
+const allPosts = async (req, res) => {
+    try {
+        const posts = await Post.find({ state: "published" })
         res.status(200).json({
-            status:"success",
+            status: "success",
             posts
         })
     } catch (error) {
@@ -47,30 +47,56 @@ const allPosts = async (req, res)=>{
     }
 }
 
-const deletePosts = async(req, res)=>{
+const deletePosts = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndRemove(req.params.postId, {
+            authorId: req.user.id
+        })
+        if (!post) return res.status(401).json({
+            success: "Fail",
+            message: 'Post with given Id not found'
+        })
 
+        if (post.authorId.toString() !== req.user.id) {
+            return res.status(401).json({
+                status: "Fail",
+                message: `You can only delete a post you created!`,
+            });
+
+        }
+
+        const postByUser = await User.findById(req.user._id);
+        postByUser.posts.pull(post._id)
+        await postByUser.updateOne({ posts: postByUser.posts })
+        res.status(200).json({
+            status: "success",
+            message: "Post deleted successfully",
+        });
+    } catch (error) {
+        throw error
+    }
 
 }
 
-const updatePosts = async(req, res)=>{
-    const {state, body} = req.body
+const updatePosts = async (req, res) => {
+    const { state, body } = req.body
     try {
         const post = await Post.findByIdAndUpdate(req.params.postId, {
-            $set:{
+            $set: {
                 state,
                 body
             }
-        }, {new:true})
+        }, { new: true })
 
-        if (post.authorId.toString() !== req.user._id) {
+        if (post.authorId.toString() !== req.user.id) {
             return res.status(401).json({
-                success:"Fail",
-                message:`You can only update a post you created!`
+                success: "Fail",
+                message: `You can only update a post you created!`
             })
         }
 
         res.status(201).json({
-            status:"Success",
+            status: "Success",
             post
         })
     } catch (error) {
@@ -79,22 +105,22 @@ const updatePosts = async(req, res)=>{
 
 }
 
-const singlePost = async(req,res)=>{
+const singlePost = async (req, res) => {
 
     try {
         const post = await Post.findById(req.params.postId).where("state").eq("published")
 
-        if(!post){
+        if (!post) {
             return res.status(404).json({
                 staus: "Failed",
                 message: "the post is not found!"
             })
         }
-        else{
-            post.readCount===0?post.readCount++:post.readCount++;
+        else {
+            post.readCount === 0 ? post.readCount++ : post.readCount++;
             await post.save()
         }
-         
+
         res.status(200).json({
             status: "success",
             post,
@@ -106,4 +132,4 @@ const singlePost = async(req,res)=>{
 
 };
 
-module.exports = {createPost,allPosts, deletePosts, updatePosts, singlePost}
+module.exports = { createPost, allPosts, deletePosts, updatePosts, singlePost }
